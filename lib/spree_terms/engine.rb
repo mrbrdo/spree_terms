ed13f4e0e7742b0287a8e00463a64b5935d90ba4
mrbrdo <mrbrdo@gmail.com>
2024-09-24 13:16:47 +0200

    fix for Zeitwerk

--- lib/spree_terms/engine.rb
+++ lib/spree_terms/engine.rb
@@ -11,7 +11,15 @@ module SpreeTerms
       g.test_framework :rspec
     end
 
+    initializer "let the main autoloader ignore this engine's overrides" do
+      overrides = root.join("app/overrides")
+      Rails.autoloaders.main.ignore(overrides)
+    end
+
     def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
+        load(c)
+      end
     end
 
     config.to_prepare &method(:activate).to_proc
